ea8bd10d63de238cf90f0a21eb538b3e64cb6cce
hubnerp <hubnerp@btlnet.com>
2019-03-05 14:56:22 +0100

    make view and sceen setting easier

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
     // poskytuje plochu pro zobrazeni vetsiho poctu QGraphicsItems
     // poskytuje collision detection
     QGraphicsScene *scene = new QGraphicsScene();
+    scene->setSceneRect(0, 0, 800, 600);
 
     // reprezentuje obdelnik, ktery je mozne pridat do sceny
     MyPlane *myPlane = new MyPlane();
@@ -21,15 +22,11 @@ int main(int argc, char *argv[])
 
     scene->addItem(myPlane);
 
+    myPlane->setPos((scene->width() - myPlane->rect().width()) / 2,
+                    scene->height() - myPlane->rect().height());
+
     // zobrazuje scenu
     QGraphicsView *view = new QGraphicsView(scene);
-    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->setFixedSize(800, 600);
-    view->setSceneRect(0, 0, 800, 600);
-
-    myPlane->setPos((view->width() - myPlane->rect().width()) / 2,
-                    view->height() - myPlane->rect().height());
 
     view->show();
 
